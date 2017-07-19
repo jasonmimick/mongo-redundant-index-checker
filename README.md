3c8cc1656bb7bc65f4d77db01a11e787aa8f612e
Jason Mimick <jmimick@gmail.com>
2017-07-19 13:55:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+mongo-redundant-index-checker
+=============================
+
+This repo has a simple tool which can parse the output of the getMongoData.js script
+to quickly let you know if a MongoDB instance has any redundant indexes.
+
+Redundant indexes are a common occurence out in the wild. This is usually a 
+consequence of multiple developers working on a common dataset or possibly
+not quite understanding how compound indexes work in MongoDB.
+
+Before showing you how to run this tool, let's define just what a redundant index
+is. 
+
+Suppose we have a collection called ``people`` and the documents look like this:
+
+{ "_id" : 3982716, 
+  "dob" : ISODate("1970-12-12"),
+  "last_name" : "Jones",
+  "first_name" : "Jimmy",
+  ...
+}
+
+Now also suppose we commonly query by names and so we defined the following 2 indexes:
+
+{ "last_name" : 1 }
+{ "last_name" : 1, "first_name" : 1 }
+
+In this case, the first index is not needed, since the 2nd index _starts with the same key set_
+as the first. Therefore, any query which could use the first index and equally use the second.
+In MongoDB queries can effeciently use any set of keys from a compound index
+provided that all keys from the beginning are used. So an index like,
+
+{ "a" : 1, "b" : 1, "c" : 1, "d" : 1}
+
+can be used for queries on:
+- "a"
+- "a" and/or "b"
+- "a", "b", "c"
+- or all 4, "a", "b", "c", "d"
+
+(and in each case and/or/nor/range/etc and any combination work fine).
+
+Ok, so how do you run this tool.
+
+First you need the output of getMongoData.js from your MongoDB instance. Then you can download and 
+run the indexChecker.js which will automatically detect the getMongoData.js output in it's 
+directory (provided it has getMongoData in it's name).
+
+More generally:
+
+$mongo <connection info> getMongoData.js > getMongoData-output-someSystem.json
+$mongo --quiet --nodb indexChecker.js
+
+Or here is a handy bash snippet you can edit with your instance connection
+information (needs ``curl``):
+
+$mongo --quiet <connection info> --eval "var _printJSON=true;$(curl -s -L https://raw.githubusercontent.com/mongodb/support-tools/master/getMongoData/getMongoData.js)" > getMongoData.output
+$mongo --quiet --nodb --eval "$(curl -s -L https://raw.githubusercontent.com/..../indexChecker.js)"
---
+++ indexChecker.js
@@ -0,0 +1,185 @@
+const util = {
+    __debug : false,
+
+    setDebug : function(on) {
+        if ( on ) {
+            __debug = true;
+        } else {
+            __debug = false;
+        }
+    },
+
+    debug : function(msg) {
+        if ( __debug ) {
+            print(msg);
+        }
+    },
+
+    loadGetMongoData : function() {
+        const fn = util.tryLoadFileName("getMongoData");
+        try {
+            return JSON.parse( util.loadFile(fn) );
+        } catch(error) {
+            util.debug("Unable to parse json in getMongoData file: " + fn);
+            try {
+                return legacyGetMongoDataParser.getJsonIndexInfo( util.loadFileLines( fn ) );
+            } catch(error2) {
+                util.debug(error2)
+                throw error2;
+            }
+        }
+    },
+    tryLoadFileName : function(name) {
+        try {
+            return ls().filter( f => f.indexOf(name) != -1 )[0];
+        } catch(error) {
+            debug("Unable to find file named '"+name+"in " + pwd());
+            throw error;
+        }
+    },
+    loadFile : function(knownFilename) {
+
+        try {
+           return cat(knownFilename);
+        } catch(error) {
+            util.debug("util.loadFile: can't cat '" + knownFilename + "'");
+            throw error;
+        }
+    },
+
+    loadFileLines : function(knownFilename) {
+        try {
+           return cat(knownFilename).split("\n");
+        } catch(error) {
+            debug("util.loadFileByLine: can't cat or split '" + knownFilename + "'");
+            throw error;
+        }
+    }
+};
+
+const legacyGetMongoDataParser = {
+
+    dumpIndexes : function(data) {
+        const i = __parseIndexes(data);
+        printjsononeline(i);
+    },
+
+    getJsonIndexInfo : function(data) {
+        return legacyGetMongoDataParser.__convertToJson(data);
+    },
+
+    __convertToJson : function(data) {
+        const i =legacyGetMongoDataParser.__parseIndexes(data);
+        return i.map( function(i) {  return { "subsection" : "indexes", "output" : i }; } );
+    },
+    __parseIndexes : function(data) {
+
+        const START_TOKEN = "** Indexes:";
+        const STOP_TOKENS = "** Collection stats (MB): ** Sample document:";
+        let write = false;
+        let indexes = [];
+        let istring = "";
+        data.filter(line => line.length>0).forEach( line => {
+            if (STOP_TOKENS.indexOf(line)!=-1) {
+                write = false;
+                util.debug('istring='+istring+ ' istring.length='+istring.length);
+                util.debug('line='+line);
+                if ( istring.length>0 ) {
+                    indexes.push( JSON.parse(istring) );
+                }
+                istring = "";
+            }
+            if (write) {
+                istring += line;
+            }
+            if (line === START_TOKEN) {
+                write = true;
+            }
+        });
+        return indexes;
+    }
+
+}
+
+const indexAnalyzer = {
+
+     getRedundant : function(getMongoDataLinesArray) {
+        const data = getMongoDataLinesArray;
+        const nsPrefixToSkip = [ 'local', 'admin', 'config' ];
+        const indexInfo = data.filter(s=>s.subsection=="indexes")
+                            .map(s=>s.output)
+                            .filter(s=> (s.length>0))
+                            .filter(s=> ( nsPrefixToSkip.indexOf(s[0].ns.split('.')[0]))==-1 );
+
+        let redundantCount = 0;
+        const redundantIndexes = {};
+        indexInfo.forEach( i => {
+            const ns = i[0].ns;
+            util.debug("Working on indexes for " + ns);
+            redundantIndexes[ ns ] = [];
+
+            i.forEach( j => {
+                const theseKeys = JSON.stringify(Object.keys( j.key )).slice(1,-1);
+                i.forEach( k => {
+                    if ( j === k ) {
+                        return;
+                    }
+                    const thoseKeys = JSON.stringify(Object.keys( k.key )).slice(1,-1);
+                    if (theseKeys.indexOf( thoseKeys ) === 0) {
+                        redundantCount ++;
+                        redundantIndexes[ ns ].push( [ k, j ] );
+                        util.debug("Detected possible redundant index: \n"
+                               + JSON.stringify(k) + "\n"
+                               + JSON.stringify(j)) + "\n";
+                    }
+                });
+
+            });
+        });
+        redundantIndexes["__count"] = redundantCount;
+        return redundantIndexes;
+     }
+
+
+}
+
+/*
+ * Expects data = {}, each key is a namespace, each value an array
+ * each array contains an array with 2 items, for the redundant indexes
+ *
+ */
+const formatter = {
+    toList : function(data) {
+        let nsCount = 1;
+        Object.keys(data).forEach( function(ns) {
+            if ( ns === "__count" ) return
+            let first = true;
+            data[ns].forEach( function(i) {
+                if ( first ) {
+                    print(ns + ":");
+                    first = false;
+                }
+                print("\t " + JSON.stringify(i[0]) + ",\t  " + JSON.stringify(i[1]));
+            });
+        });
+    }
+}
+
+const main = {
+
+    main : function() {
+
+        try {
+            util.setDebug(false);
+            const data = util.loadGetMongoData();
+            const result = indexAnalyzer.getRedundant(data);
+            util.debug(result);
+            formatter.toList(result);
+        } catch(error) {
+            printjson(error);
+
+        }
+    }
+};
+
+main.main();
